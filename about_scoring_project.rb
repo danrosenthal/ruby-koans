ad9f017b7042d4b918ad4a9ad72bf4b4136d991c
Dan Rosenthal <dan@heysparkbox.com>
2015-01-22 13:00:33 -0500

    Comment out scoring debuggers

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -31,18 +31,18 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 def score(dice)
   dice.sort!
-  puts "Roll: " + dice.inspect
+  #puts "Roll: " + dice.inspect
   return 0 if(dice == [] || dice == nil)
   return 1000 + score(dice[3..-1]) if(dice[0..2] == [1,1,1])
-  puts dice.inspect  
+  #puts dice.inspect  
   return (dice[0] * 100) + score(dice[3..-1]) if(dice[0] == dice[2])
-  puts dice.inspect
+  #puts dice.inspect
   return 50 + score(dice[1..-1]) if(dice[0] == 5)
-  puts dice.inspect
+  #puts dice.inspect
   return 100 + score(dice[1..-1]) if(dice[0] == 1)
-  puts dice.inspect
+  #puts dice.inspect
   return 0 + score(dice[1..-1])  
-  puts dice.inspect
+  #puts dice.inspect
 end
 
 class AboutScoringProject < Neo::Koan
